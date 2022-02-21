cf8bcf41beb440557dde62d50eff539f6b970e8c
Daren Danganan <daren.danganan@icloud.com>
2022-02-21 09:33:34 -0600

    more options

--- lua/settings.lua
+++ lua/settings.lua
@@ -3,15 +3,14 @@ local wo = vim.wo
 local bo = vim.bo
 
 -- global options
+	-- swap file options
 o.swapfile = true	-- enable swap files
 o.dir = '/tmp'		-- store swap files here
+	-- find options
 o.ignorecase = true 	-- ignore case when searching
-o.smartcase = true	--[[ ignore case on find if no 
-				Uppercase present only 
-				enabled if ignorecase 
-				is true
-			--]]
-o.laststatus = 2	-- always show status line
+o.smartcase = true	
 o.hlsearch = true 	-- enable search highlights
 o.incsearch = true	-- enable search as you type
-
+	-- display options
+o.wrap = false		-- disable wrap
+o.laststatus = 2	-- always show status line
