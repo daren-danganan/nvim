172aa872827a50bef8abbfc0557a5b795374bd1c
Daren Danganan <daren.danganan@icloud.com>
2022-03-04 11:24:07 -0600

    add colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -39,8 +39,9 @@ return packer.startup(function(use)
   use 'wbthomason/packer.nvim' -- Have packer manage itself
   use "nvim-lua/popup.nvim" --[[ An implementation of the Popup API from vim 
                                 in Neovim --]]
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
   use "neovim/nvim-lspconfig"
+  use "Mofiqul/vscode.nvim" -- VSCode Colorscheme
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
