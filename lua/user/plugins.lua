83cf460a7987d170b9658d790eca874bca80bd67
Daren Danganan <daren.danganan@icloud.com>
2022-03-04 20:49:52 -0600

    add completions

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -35,13 +35,29 @@ packer.init({
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
+  -- Plugin Installer
   use 'wbthomason/packer.nvim' -- Have packer manage itself
+
+  -- Utilities
   use "nvim-lua/popup.nvim" --[[ An implementation of the Popup API from vim 
                                 in Neovim --]]
   use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
+
+  -- LSP
   use "neovim/nvim-lspconfig"
+
+  -- Colorschemes
   use "Mofiqul/vscode.nvim" -- VSCode Colorscheme
+
+  -- Completions
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+
+  -- -- Completion Snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
