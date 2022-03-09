8882f54aaa2f137561edce020f12091a7b375157
Daren Danganan <daren.danganan@icloud.com>
2022-03-08 18:48:34 -0600

    fix issue with lsp

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ require('user.keymaps')         --lua/user/keymaps.lua
 require('user.plugins')         --lua/user/plugins.lua
 require('user.colorschemes')    --lua/user/colorschemes.lua
 require('user.completions')     --lua/user/completions.lua
+require('user.lsp')
 require('user.telescope')       --lua/user/telescope.lua
 require('user.treesitter')      --lua/user/treesitter.lua
