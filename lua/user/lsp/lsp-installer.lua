bee26d33dd03309d931ed9948b9ad1d9e19fdf53
Daren Danganan <daren.danganan@icloud.com>
2022-03-11 13:34:28 -0600

    add openapi 3.0 yaml support

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -6,13 +6,13 @@ end
 
 -- change default lsp installer icons
 lsp_installer.settings({
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
     }
+  }
 })
 
 -- Register a handler that will be called for all installed servers.
@@ -27,6 +27,17 @@ lsp_installer.on_server_ready(function(server)
     local sumneko_opts = require("user.lsp.settings.sumneko_lua")
     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
   end
+
+  if server.name == "jsonls" then
+    local jsonls_opts = require("user.lsp.settings.jsonls")
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
+
+  if server.name == "yamlls" then
+    local yamlls_opts = require("user.lsp.settings.yamlls")
+    opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
+  end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
