8bfd4117191d7b4f5614824032a2d5c120284173
Daren Danganan <daren.danganan@icloud.com>
2022-03-11 18:07:54 -0600

    revert find files to default

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -41,6 +41,5 @@ km("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- Telescope
 km("n", "<leader>f", "<cmd>lua require'telescope.builtin'"
-                    .. ".find_files(require('telescope.themes')"
-                        .. ".get_dropdown({ previewer = false }))<cr>", opts)
+  .. ".find_files()<cr>", opts)
 km("n", "<leader>g", "<cmd>Telescope live_grep<cr>", opts)
