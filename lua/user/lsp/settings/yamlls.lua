e2a428d0fd4a29419136e7fbbaa69c36e6f69571
Daren Danganan <daren.danganan@icloud.com>
2022-03-29 23:03:45 -0500

    simplify yamlls setup

--- lua/user/lsp/settings/yamlls.lua
+++ lua/user/lsp/settings/yamlls.lua
@@ -1,27 +1,12 @@
-local default_schemas = nil
-local status_ok, yamlls_settings = pcall(require, 'nlspsettings.loaders.yaml')
-if status_ok then
-  default_schemas = yamlls_settings.get_default_schemas()
-end
-
 local schemas = {
   -- ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.yaml"] = "*.openapi.yaml",
-  ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.yaml"] = "*.openapi.yaml",
+  ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.yaml"] = "*.yaml",
 }
-
-local function extend(tab1, tab2)
-  for _, value in ipairs(tab2) do
-    table.insert(tab1, value)
-  end
-  return tab1
-end
-
-local extended_schemas = extend(schemas, default_schemas)
 
 local opts = {
   settings = {
     yaml = {
-      schemas = extended_schemas,
+      schemas = schemas,
     },
   },
   setup = {
